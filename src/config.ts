1feeb62a327065f384afe4d8ffb407f10fa06450
bashurova.olga <bashurova.olga@gmail.com>
2023-07-03 12:35:59 +0300

    fixed default url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const getLink = (env_link: string) => process.env[env_link] || `https://dummyjson.com`
+const getLink = (env_link: string) => process.env[env_link] || `https://dummyjson.com/products`
 
 export const API = {
   products: getLink('REACT_APP_PRODUCTS_API_URL'),
